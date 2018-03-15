c3163e6d795b36357f8300930660ab7171b856a9
alfonsohernandez <hernandezharoalfonso@gmail.com>
2018-03-15 13:18:23 +0100

    Testing Github

--- app/src/main/java/com/example/alfonsohernandez/yoyocinema/MainActivity.kt
+++ app/src/main/java/com/example/alfonsohernandez/yoyocinema/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val numero : Int = 0
+        val numero2 : Int = 0
     }
 }
